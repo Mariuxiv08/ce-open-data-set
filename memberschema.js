0979998b9afd981fc9a01eb7b697eb7b3e3b3fdf
Mariuxi <mariuxiv08@gmail.com>
2018-07-23 14:37:22 -0400

    Created member schema

---
+++ memberschema.js
@@ -0,0 +1,44 @@
+{
+  "BigQuery Schema": [
+    {
+      "name": "Member_ID",
+      "type": "NUMERIC"
+    },
+    {
+      "name": "First_Name",
+      "type": "STRING"
+    },
+    {
+      "name": "Last_Name",
+      "type": "STRING"
+    },
+    {
+      "name": "Gender",
+      "type": "STRING"
+    },
+    {
+      "name": "Age",
+      "type": "NUMERIC"
+    },
+    {
+     "name": "Height",
+      "type": "NUMERIC"
+    },
+    {
+     "name": "Weight",
+      "type": "NUMERIC"
+    },
+    {
+     "name": "Hours_Sleep",
+      "type": "NUMERIC"
+    },
+    {
+     "name": "Exercise_Calories_Burned",
+      "type": "NUMERIC"
+    },
+    {
+     "name": "Date",
+      "type": "NUMERIC"
+    }
+  ]
+}
