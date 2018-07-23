add1596dee95adbfaaf5db6197be033d690b8594
Mariuxi <mariuxiv08@gmail.com>
2018-07-23 14:40:31 -0400

    Created member function js

---
+++ memberfunction.js
@@ -0,0 +1,19 @@
+function transform(line) {
+var values = line.split(',');
+
+var obj = new Object();
+obj.Member_ID = values[0];
+obj.First_Name = values[1];
+obj.Last_Name = values[2];
+obj.Gender = values[3];
+obj.Age = values[4];
+obj.Height = values[5];
+obj.Weight = values[6];
+obj.Hours_Sleep = values[7];
+obj.Calories_Consumed = values[8];
+obj.Exercise_Calories_Burned = values[9];
+obj.Date = values[10];
+var jsonString = JSON.stringify(obj);
+
+return jsonString;
+}
